ebbdd82b8b576db017c41d0358ac3eee0400153b
ignacio1997img <ignaciomolinaguzman20@gmail.com>
2023-02-28 12:39:51 -0400

    si

--- resources/views/vendor/voyager/index.blade.php
+++ resources/views/vendor/voyager/index.blade.php
@@ -5,10 +5,27 @@
         @include('voyager::alerts')
         @include('voyager::dimmers')
         <div class="analytics-container">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn" onclick="download_log('Guia Telefonica')" style="border-radius: 20px; height:300px; width: 280px; background-color: #08acf2; color:#ffffff; font-size: 50px" data-toggle="modal" >
+                        <i class="fa-solid fa-file-pdf"></i><span class="hidden-xs hidden-sm"><br> Gia <br> Telefonica</span>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn" onclick="download_log('Guia Telefonica')" style="border-radius: 20px; height:300px; width: 280px; background-color: #08acf2; color:#ffffff; font-size: 50px" data-toggle="modal" >
+                        <i class="fa-solid fa-file-pdf"></i><span class="hidden-xs hidden-sm"><br> Gia <br> Telefonica</span>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn" onclick="download_log('Guia Telefonica')" style="border-radius: 20px; height:300px; width: 280px; background-color: #08acf2; color:#ffffff; font-size: 50px" data-toggle="modal" >
+                        <i class="fa-solid fa-file-pdf"></i><span class="hidden-xs hidden-sm"><br> Gia <br> Telefonica</span>
+                    </a>
+                </div>
 
-            <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn btn-info" onclick="download_log('Guia Telefonica')" data-toggle="modal" >
-                <i class="fa-solid fa-file-pdf"></i><span class="hidden-xs hidden-sm"><br> Gia Telefonica</span>
-            </a>
+            </div>
+
+            
+            
         </div>
     </div>
 @stop
