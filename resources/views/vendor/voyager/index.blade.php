897e8ae6500e8e2752e141f17d42d9c912cdc7fd
ignacio1997img <ignaciomolinaguzman20@gmail.com>
2023-02-28 13:08:44 -0400

    auth

--- resources/views/vendor/voyager/index.blade.php
+++ resources/views/vendor/voyager/index.blade.php
@@ -5,20 +5,20 @@
         @include('voyager::alerts')
         @include('voyager::dimmers')
         <div class="analytics-container">
-            <div class="row">
-                <div class="col-md-4">
-                    <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn" onclick="download_log('Guia Telefonica')" style="border-radius: 20px; height:300px; width: 280px; background-color: #08acf2; color:#ffffff; font-size: 50px" data-toggle="modal" >
-                        <i class="fa-solid fa-file-pdf"></i><span class="hidden-xs hidden-sm"><br> Gia <br> Telefonica</span>
+            <div class="row" style="text-align: center">
+                <div class="col-md-4" >
+                    <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn" onclick="download_log('Guia Telefonica')" style="margin-top: 1em; border-radius: 20px; height:300px; width: 250px; background-color: #08acf2; color:#ffffff; font-size: 30px" data-toggle="modal" >
+                        <i class="fa-solid fa-file-pdf" style="color: #ffffff; font-size: 4em;"></i><span class="hidden-xs hidden-sm"><br> Gia <br> Telefonica</span>
                     </a>
                 </div>
                 <div class="col-md-4">
-                    <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn" onclick="download_log('Guia Telefonica')" style="border-radius: 20px; height:300px; width: 280px; background-color: #08acf2; color:#ffffff; font-size: 50px" data-toggle="modal" >
-                        <i class="fa-solid fa-file-pdf"></i><span class="hidden-xs hidden-sm"><br> Gia <br> Telefonica</span>
+                    <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn" onclick="download_log('Guia Telefonica')" style="margin-top: 1em; border-radius: 20px; height:300px; width: 250px; background-color: #08acf2; color:#ffffff; font-size: 30px" data-toggle="modal" >
+                        <i class="fa-solid fa-file-pdf" style="color: #ffffff; font-size: 4em;"></i><span class="hidden-xs hidden-sm"><br> Gia <br> Telefonica</span>
                     </a>
                 </div>
                 <div class="col-md-4">
-                    <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn" onclick="download_log('Guia Telefonica')" style="border-radius: 20px; height:300px; width: 280px; background-color: #08acf2; color:#ffffff; font-size: 50px" data-toggle="modal" >
-                        <i class="fa-solid fa-file-pdf"></i><span class="hidden-xs hidden-sm"><br> Gia <br> Telefonica</span>
+                    <a href="{{ asset('pdf/GUIA.pdf') }}" target="_blank" title="Descargar Guia" class="btn" onclick="download_log('Guia Telefonica')" style="margin-top: 1em; border-radius: 20px; height:300px; width: 250px; background-color: #08acf2; color:#ffffff; font-size: 30px" data-toggle="modal" >
+                        <i class="fa-solid fa-file-pdf" style="color: #ffffff; font-size: 4em;"></i><span class="hidden-xs hidden-sm"><br> Gia <br> Telefonica</span>
                     </a>
                 </div>
 
