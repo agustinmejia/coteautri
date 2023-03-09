7916e0121335b44f35efa0dfdadae53069c975be
ignacio1997img <ignaciomolinaguzman20@gmail.com>
2023-03-08 21:53:23 -0400

    consulta de saldo

--- resources/views/debtor/browse.blade.php
+++ resources/views/debtor/browse.blade.php
@@ -205,7 +205,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title"><i class="voyager-basket"></i> Detalles</h4>
+                        <h4 class="modal-title"><i class="fa-solid fa-list"></i> Detalles</h4>
                     </div>
                     <div class="modal-body">
                         <div class="row">
